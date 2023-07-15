d3fbed4239916b3987be9f38ed57e4ec62891c5f
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2023-07-16 02:19:16 +0300

    feat [HomePage] : minor type change

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import { AddCircle } from '@styled-icons/fluentui-system-filled/'
 import Modal from '../components/Modal/Modal'
 
 export default function HomaPage() {
-  const [bookID, setBookID] = useState('')
+  const [bookID, setBookID] = useState<any>()
   const [showModal, setShowModal] = useState(false)
 
   return (
@@ -27,11 +27,11 @@ export default function HomaPage() {
 }
 
 /*---> Styles <---*/
-export const PageWrapper = styled.div`
+const PageWrapper = styled.div`
   /* border: 1px solid red; */
 `
 
-export const Title = styled.div`
+const Title = styled.div`
   /* border: 1px solid red; */
   font-size: 50px;
   font-weight: bold;
@@ -39,24 +39,24 @@ export const Title = styled.div`
   margin-top: 20px;
 `
 
-export const ContentWrapper = styled.div`
+const ContentWrapper = styled.div`
   /* border: 1px solid yellow; */
   display: flex;
 `
 
-export const LeftWrapper = styled.div`
+const LeftWrapper = styled.div`
   /* border: 1px solid green; */
   width: 65%;
   padding: 30px;
 `
 
-export const RightWrapper = styled.div`
+const RightWrapper = styled.div`
   /* border: 1px solid black; */
   width: 35%;
   padding: 30px;
 `
 
-export const AddIcon = styled(AddCircle)`
+const AddIcon = styled(AddCircle)`
   /* border: 1px solid black; */
   width: 70px;
   position: fixed;
